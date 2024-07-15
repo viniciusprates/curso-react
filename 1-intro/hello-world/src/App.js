0cbc80ad78478ba39aa508232815cee17e8453cc
vinialmeida457@gmail.com <vinialmeida457@gmail.com>
2024-07-15 18:36:50 -0300

    Tarefa 1

---
+++ 1-intro/hello-world/src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React</h1>
+      <h2>H2</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+        aperiam alias harum ipsam fugit. Vitae modi vel eius corporis earum,
+        doloribus quaerat officiis assumenda voluptatem iure. Omnis itaque
+        tenetur sunt.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+        aperiam alias harum ipsam fugit. Vitae modi vel eius corporis earum,
+        doloribus quaerat officiis assumenda voluptatem iure. Omnis itaque
+        tenetur sunt.
+      </p>
+    </div>
+  );
+}
+
+export default App;
